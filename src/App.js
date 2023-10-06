fac748dea88200a59dc03238bc6ac982b90561f1
aulanbekuulu <aulanbekuulu@srs.lan>
2023-10-06 12:40:23 +0600

    2. Redux и React. State, reducer, action + redux hooks useDispatch и useSelector

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash)
+
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash})
+    }
+
+    return (
     <div className="App">
+        <div style={{fontSize:'3rem'}}>{cash}</div>
       <div style={{display:'flex'}}>
-        <button>Попонить счет</button>
-        <button>Снять со счета</button>
+        <button onClick={() => addCash(Number(prompt()))}>Попонить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
       </div>
     </div>
-  );
+    );
 }
 
 export default App;
