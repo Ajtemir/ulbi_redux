d50cffc3959ff1816644e0e9d730a35f0618e54e
aulanbekuulu <aulanbekuulu@srs.lan>
2023-10-09 13:01:36 +0600

    5. React и Redux.Action creators. Redux thunk и асинхронные действия

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
+import {fetchCustomers} from "./asyncActions/customers";
 
 function App() {
     const dispatch = useDispatch()
@@ -35,7 +36,7 @@ function App() {
         <button onClick={() => addCash(Number(prompt()))}>Попонить счет</button>
         <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
         <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
-        <button onClick={() => getCash(Number(prompt()))}>Удалить клиента</button>
+        <button onClick={() => dispatch(fetchCustomers())}>Получить клиентов из базы</button>
       </div>
         <div>
             {customers.length > 0
